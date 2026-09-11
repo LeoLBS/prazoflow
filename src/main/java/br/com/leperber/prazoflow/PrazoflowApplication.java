3681ce7b1609a726b2d0350d64630c7db7342713
LeoLBS <leonardoperin70@gmail.com>
2026-09-10 22:36:37 -0300

    chore: adiciona esqueleto inicial do Spring Boot

---
+++ src/main/java/br/com/leperber/prazoflow/PrazoflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.leperber.prazoflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrazoflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrazoflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leperber/prazoflow/PrazoflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leperber.prazoflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrazoflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
