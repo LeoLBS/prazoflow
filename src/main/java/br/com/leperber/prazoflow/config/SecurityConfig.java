2dc3374dc9f85d36aa752609e71ca817cc201a3f
LeoLBS <leonardoperin70@gmail.com>
2026-09-18 14:05:01 -0300

    feat: adiciona configuracao inicial de PasswordEncoder com BCrypt

---
+++ src/main/java/br/com/leperber/prazoflow/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package br.com.leperber.prazoflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
