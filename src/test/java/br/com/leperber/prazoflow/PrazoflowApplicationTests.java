6d8a946d6e2c1fb2f9988d16572b233041e44330
LeoLBS <leonardoperin70@gmail.com>
2026-09-21 09:09:22 -0300

    test: mocka notificador Discord no teste de contexto

--- src/test/java/br/com/leperber/prazoflow/PrazoflowApplicationTests.java
+++ src/test/java/br/com/leperber/prazoflow/PrazoflowApplicationTests.java
@@ -1,10 +1,15 @@
 package br.com.leperber.prazoflow;
 
+import br.com.leperber.prazoflow.bot.DiscordNotificador;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(properties = "discord.enabled=false")
 class PrazoflowApplicationTests {
+
+	@MockitoBean
+    DiscordNotificador discordNotificador;
 
 	@Test
 	void contextLoads() {
