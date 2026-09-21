47d052ebee2edf7a7f61b67919efca7b55ae8c90
LeoLBS <leonardoperin70@gmail.com>
2026-09-21 06:54:44 -0300

    test: desabilita bot Discord no teste de contexto

--- src/test/java/br/com/leperber/prazoflow/PrazoflowApplicationTests.java
+++ src/test/java/br/com/leperber/prazoflow/PrazoflowApplicationTests.java
@@ -3,7 +3,7 @@ package br.com.leperber.prazoflow;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "discord.enabled=false")
 class PrazoflowApplicationTests {
 
 	@Test
